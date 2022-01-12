2a98e683f597b1f04e64ad4f6ce73faa5f2ed65c
mclear73 <mclear73@gmail.com>
2022-01-12 12:03:21 -0500

    reformated wiki

--- Wiki.md
+++ Wiki.md
@@ -1,27 +1,31 @@
 # Overview of *M. truncatula* RNAseq and GWAS analyses and location of source code and data files
 The goal of this wiki is to provide a road map for data analysis files, figures, and source code. For a more in-depth description of how to run these analyses please see my [gitlab project here](https://gitlab.com/mclear73/medicago-rnaseq).  
-## Description of available RNAseq data files (as of January 10, 2022)
-### Input/Reference Files
-* [Compiled Raw Count file](https://www.dropbox.com/s/47uxns3am4ywxgv/All_Counts_Raw.csv?dl=0)
-* [GMT File for Running GO Term Enrichment](https://www.dropbox.com/s/ikzcd1c5eewqaa5/mtruncatula_v5.gmt?dl=0)
-* [Version 5 annotation file (.gff3)](https://www.dropbox.com/s/nywnkx7pdw9iijk/MtrunA17r5.0-ANR-EGN-r1.8.gff3?dl=0)
-* [Version 5 assembly file (.fasta)](https://www.dropbox.com/s/3ejs3e3jkpig2f9/mtruna17r5.0-20161119-anr.genome.fasta?dl=0)
-* [Compiled Normalized Transcripts per Million (TPM) Count file](https://www.dropbox.com/s/29gp73jzqyn2ylu/All_Counts_TPM.csv?dl=0)
-* [Compiled Normalized Fragments per Kilobase Million (FPKM) Count file](https://www.dropbox.com/s/7aemxhmlq58osst/All_Counts_FPKM.csv?dl=0)
-* [Metadata associated with samples](https://www.dropbox.com/s/mzc74y7vrl0hcr7/allMetadata.csv?dl=0)
-* [Compiled annotation for *M. truncatula*](https://www.dropbox.com/s/hd1k3dnje2uc740/M_truncV5.csv?dl=0). Includes:
-	* v4 and v5 gene names
-	* Pfam domain, KOG, KEGG, GO, Panther, Interpro annotations
-	* [Arabidopsis thaliana top blast hit](https://www.dropbox.com/s/fhkfrut18517bkh/BLAST2_Athaliana_output.txt?dl=0) and gene description
-	* Plant TFDB transcription factor family (if identified as a transcription factor)
-* [Name conversion file between v4 and v5 genomes](https://www.dropbox.com/s/kq4dkxjze7sgoxl/MtrunA17r5.0-ANR_geneIDs.txt?dl=0)
-### Compiled DEGs with annotation
-* [Compiled differentially expressed gene (DEG) file](https://www.dropbox.com/s/m0dgyjnfmj9dm2m/Compiled_DEGs_wAnnotation.csv?dl=0). Includes:
-	*  DEGs of treatment versus control samples provided by JGI
-	*  DEGs of combined treatments (ex: all Hg-treated root samples)
-	*  *M. truncatula* annotation
-	*  Assigned Co-expression module for root and leaf co-expression networks (determined by WGCNA)
-*  [Overenriched GO-terms in DEG by treatment](https://www.dropbox.com/s/a061h31e1hp07b2/gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)
+## Description and Location of available RNAseq data files (as of January 11, 2022)
+### Input/Reference Files used by JGI for mapping raw reads to the genome
+* [Version 5 annotation file (.gff3)](https://www.dropbox.com/s/nywnkx7pdw9iijk/MtrunA17r5.0-ANR-EGN-r1.8.gff3?dl=0) - taken from the *M. truncatula v5* genome website; reads were mapped to this
+* [Version 5 assembly file (.fasta)](https://www.dropbox.com/s/3ejs3e3jkpig2f9/mtruna17r5.0-20161119-anr.genome.fasta?dl=0) - taken from the *M. truncatula v5* genome website; reads were mapped to this
+* [Metadata associated with samples](https://www.dropbox.com/s/mzc74y7vrl0hcr7/allMetadata.csv?dl=0) - description of the different samples; used by JGI to determine DESeq comparisons
+
+### Output files from JGI (raw and normalized counts)
+* [Compiled Raw Count file](https://www.dropbox.com/s/47uxns3am4ywxgv/All_Counts_Raw.csv?dl=0) - Raw count output per gene
+* [Compiled Normalized Transcripts per Million (TPM) Count file](https://www.dropbox.com/s/29gp73jzqyn2ylu/All_Counts_TPM.csv?dl=0)- normalized counts
+* [Compiled Normalized Fragments per Kilobase Million (FPKM) Count file](https://www.dropbox.com/s/7aemxhmlq58osst/All_Counts_FPKM.csv?dl=0)- normalized counts
+
+### Input files for generating the full annotation file 
+* [Protein fasta file](https://www.dropbox.com/s/rmsypc4p6lnquwj/mtruna17r5.0-20161119-anr.genome_protein.fasta?dl=0) - this was generated using the [funnanotate](https://funannotate.readthedocs.io/en/latest/) gff2prot function and the assembly and annotation as inputs
+* [v5 annotation file](https://medicago.toulouse.inra.fr/MtrunA17r5.0-ANR/). Includes:
+	* pfam domain, KOG, KEGG, GO, Panther, Interpro annotations 
+* [Top hits for BLAST  to *A. thaliana* genome](https://www.dropbox.com/s/hd1k3dnje2uc740/M_truncV5.csv?dl=0) - generated using blastp function from the [ncbi tools package](https://www.ncbi.nlm.nih.gov/books/NBK279690/) and bash script for parsing the output
+* Plant TFDB transcripton factor family - predicted using the [plant TFDB prediction tool](http://planttfdb.gao-lab.org/prediction.php); predicts plant transciption factors
+* signalP5.0 output - generated using the protein fasta file and the [signalP api](https://services.healthtech.dtu.dk/service.php?SignalP-5.0); this predicts whether or not a protein is secreted
+* [Name conversion file between v4 and v5 genomes](https://www.dropbox.com/s/kq4dkxjze7sgoxl/MtrunA17r5.0-ANR_geneIDs.txt?dl=0) - allows us to convert between v4 and v5 gene names
+
+### Final annotation
+* [Final Compiled annotation for *M. truncatula*](https://www.dropbox.com/s/hd1k3dnje2uc740/M_truncV5.csv?dl=0) - this will be attached to other statistical outputs such as differentially expressed genes so that biologically relevant information can be inferred
+
+### Input file for gene set enrichment (GO-term enrichment)
+* [GMT File for Running GO Term Enrichment](https://www.dropbox.com/s/ikzcd1c5eewqaa5/mtruncatula_v5.gmt?dl=0) - this was produced using the GO annotation provided by the v5 genome group and my own python script
+
 ### WGCNA Co-expression Output
 *  Leaf co-expression network determined by WGCNA (Probably better to look at the visualization for this information, however the raw node/edge data is deposited here)
 	* Leaf WGCNA Node
@@ -29,9 +33,23 @@ The goal of this wiki is to provide a road map for data analysis files, figures,
 *  Root co-expression network determined by WGCNA (Probably better to look at the visualization for this information, however the raw node/edge data is deposited here)
 	* Root WGCNA Node
 	* Root WGCNA Edge
-*  Overenriched GO-terms within co-expression modules
-	* [Root Revigo output](https://www.dropbox.com/s/q4323sq18jeqm93/Root_WGCNA_gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)
-	* [Leaf Revigo output](https://www.dropbox.com/s/f9jmnclgjess47d/Leaf_WGCNA_gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)  
+
+	
+### Overenriched GO-terms within co-expression modules
+* [Root Revigo output](https://www.dropbox.com/s/q4323sq18jeqm93/Root_WGCNA_gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)
+* [Leaf Revigo output](https://www.dropbox.com/s/f9jmnclgjess47d/Leaf_WGCNA_gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)  
+
+### Compiled DEGs with annotation
+* [Compiled differentially expressed gene (DEG) file](https://www.dropbox.com/s/m0dgyjnfmj9dm2m/Compiled_DEGs_wAnnotation.csv?dl=0). Includes:
+	*  DEGs of treatment versus control samples provided by JGI
+	*  DEGs of combined treatments (ex: all Hg-treated root samples)
+	*  *M. truncatula* annotation
+	*  Averaged (by treatment) TPM counts
+	*  Assigned Co-expression module for root and leaf co-expression networks (determined by WGCNA)
+
+### Overenriched GO-terms determined using DEGs
+*  [Overenriched GO-terms in DEG by treatment](https://www.dropbox.com/s/a061h31e1hp07b2/gProfiler_GO_enrichment_REVIGO_FULL.csv?dl=0)
+
 ### Gene Lists
 *  Top gene list
 
---
+++ scripts/roots_WGCNA.R
@@ -0,0 +1,364 @@
+#=====================================================================================
+#
+#  Code chunk 1
+#
+#=====================================================================================
+
+
+# Display the current working directory
+dir <- getwd()
+setwd(dir)
+
+# Load the WGCNA package
+library(WGCNA);
+# The following setting is important, do not omit.
+options(stringsAsFactors = FALSE);
+#Read in the female liver data set
+#Read in the female liver data set
+femData = read.csv("All_Medicago_Counts.csv");
+# Take a quick look at what is in the data set:
+#dim(femData);
+#names(femData);
+femMeta = read.csv("allMetadata.csv")
+femMeta = femMeta[ which(femMeta$TissueType=='Root'), ]
+sampList = femMeta$Samples
+sampList = c("Gene", sampList)
+femData = femData[ , sampList]
+
+
+#=====================================================================================
+#
+#  Code chunk 2
+#
+#=====================================================================================
+
+
+datExpr0 = as.data.frame(t(femData[, -c(1)]));
+names(datExpr0) = femData$Gene;
+rownames(datExpr0) = names(femData)[-c(1)];
+#dim(datExpr0)
+print("printing rownames")
+print(rownames(datExpr0))
+
+#=====================================================================================
+#
+#  Code chunk 3
+#
+#=====================================================================================
+
+
+gsg = goodSamplesGenes(datExpr0, verbose = 3);
+gsg$allOK
+
+
+#=====================================================================================
+#
+#  Code chunk 4
+#
+#=====================================================================================
+
+
+if (!gsg$allOK)
+{
+  # Optionally, print the gene and sample names that were removed:
+  if (sum(!gsg$goodGenes)>0) 
+    printFlush(paste("Removing genes:", paste(names(datExpr0)[!gsg$goodGenes], collapse = ", ")));
+  if (sum(!gsg$goodSamples)>0) 
+    printFlush(paste("Removing samples:", paste(rownames(datExpr0)[!gsg$goodSamples], collapse = ", ")));
+  # Remove the offending genes and samples from the data:
+  datExpr0 = datExpr0[gsg$goodSamples, gsg$goodGenes]
+}
+
+
+#=====================================================================================
+#
+#  Code chunk 5
+#
+#=====================================================================================
+
+
+sampleTree = hclust(dist(datExpr0), method = "average");
+# Plot the sample tree: Open a graphic output window of size 12 by 9 inches
+# The user should change the dimensions if the window is too large or too small.
+sizeGrWindow(12,9)
+pdf(file = "Plots/Root_PlantsampleClustering.pdf", width = 12, height = 9);
+par(cex = 0.6);
+par(mar = c(0,4,2,0))
+plot(sampleTree, main = "Plant_Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5, 
+     cex.axis = 1.5, cex.main = 2)
+
+
+#=====================================================================================
+#
+#  Code chunk 6
+#
+#=====================================================================================
+
+
+# Plot a line to show the cut
+abline(h = 280, col = "red");
+# Determine cluster under the line
+clust = cutreeStatic(sampleTree, cutHeight = 150, minSize = 10)
+table(clust)
+# clust 1 contains the samples we want to keep.
+keepSamples = (clust==1)
+datExpr = datExpr0[keepSamples, ]
+nGenes = ncol(datExpr)
+nSamples = nrow(datExpr)
+dev.off()
+
+#=====================================================================================
+#
+#  Code chunk 7
+#
+#=====================================================================================
+
+
+#traitData = read.csv("WGCNA counts PlantMeta2.csv");
+#dim(traitData)
+#names(traitData)
+
+# remove columns that hold information we do not need.
+#allTraits = traitData[, -c(6, 7)];
+#allTraits = allTraits[, c(2, 11:36) ];
+#dim(allTraits)
+#names(allTraits)
+
+# Form a data frame analogous to expression data that will hold the clinical traits.
+
+#femaleSamples = rownames(datExpr);
+#traitRows = match(femaleSamples, allTraits$File);
+#datTraits = allTraits[traitRows, -1];
+#rownames(datTraits) = allTraits[traitRows, 1];
+
+#collectGarbage();
+
+
+#=====================================================================================
+#
+#  Code chunk 8
+#
+#=====================================================================================
+
+
+# Re-cluster samples
+#sampleTree2 = hclust(dist(datExpr), method = "average")
+# Convert traits to a color representation: white means low, red means high, grey means missing entry
+#traitColors = numbers2colors(datTraits, signed = FALSE);
+# Plot the sample dendrogram and the colors underneath.
+#plotDendroAndColors(sampleTree2, traitColors,
+#                    groupLabels = names(datTraits), 
+#                    main = "Sample dendrogram and trait heatmap")
+
+
+#=====================================================================================
+#
+#  Code chunk 9
+#
+#=====================================================================================
+
+
+save(datExpr, file = "Root-01-dataInput.RData")
+
+############################################################################
+#Step-by-step network construction
+#=====================================================================================
+#
+#  Code chunk 1
+#
+#=====================================================================================
+
+
+# Display the current working directory
+dir <- getwd()
+setwd(dir)
+
+# The following setting is important, do not omit.
+options(stringsAsFactors = FALSE);
+# Allow multi-threading within WGCNA. At present this call is necessary.
+# Any error here may be ignored but you may want to update WGCNA if you see one.
+# Caution: skip this line if you run RStudio or other third-party R environments.
+# See note above.
+enableWGCNAThreads()
+# Load the data saved in the first part
+lnames = load(file = "Root-01-dataInput.RData");
+#The variable lnames contains the names of loaded variables.
+lnames
+
+
+#=====================================================================================
+#
+#  Code chunk 2
+#
+#=====================================================================================
+
+
+# Choose a set of soft-thresholding powers
+powers = c(c(1:10), seq(from = 12, to=30, by=2))
+# Call the network topology analysis function
+sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+# Plot the results:
+sizeGrWindow(9, 5)
+par(mfrow = c(1,2));
+cex1 = 0.9;
+pdf(file = "Plots/Root_SoftThresholdvScaleFreeToplogy.pdf", width = 12, height = 9);
+# Scale-free topology fit index as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+     main = paste("Scale independence"));
+text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels=powers,cex=cex1,col="red");
+# this line corresponds to using an R^2 cut-off of h
+abline(h=0.90,col="red")
+dev.off()
+pdf(file = "Plots/Root_SoftThresholdvsMeanConnectivity.pdf", width = 12, height = 9);
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], sft$fitIndices[,5],
+     xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+     main = paste("Mean connectivity"))
+text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
+dev.off()
+
+#=====================================================================================
+#
+#  Code chunk 3
+#
+#=====================================================================================
+
+
+softPower = 10;
+adjacency = adjacency(datExpr, power = softPower);
+
+
+#=====================================================================================
+#
+#  Code chunk 4
+#
+#=====================================================================================
+
+
+# Turn adjacency into topological overlap
+TOM = TOMsimilarity(adjacency);
+dissTOM = 1-TOM
+
+save(dissTOM, file = "Root-01-dissTOM.RData")
+#=====================================================================================
+#
+#  Code chunk 5
+#
+#=====================================================================================
+
+
+# Call the hierarchical clustering function
+geneTree = hclust(as.dist(dissTOM), method = "average");
+# Plot the resulting clustering tree (dendrogram)
+sizeGrWindow(12,9)
+pdf(file = "Plots/Root_Gene Clustering.pdf", width = 12, height = 9);
+plot(geneTree, xlab="", sub="", main = "Gene clustering on TOM-based dissimilarity",
+     labels = FALSE, hang = 0.04);
+
+#=====================================================================================
+#
+#  Code chunk 6
+#
+#=====================================================================================
+
+
+# We like large modules, so we set the minimum module size relatively high:
+minModuleSize = 30;
+# Module identification using dynamic tree cut:
+dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
+                            deepSplit = 2, pamRespectsDendro = FALSE,
+                            minClusterSize = minModuleSize);
+table(dynamicMods)
+dev.off()
+
+#=====================================================================================
+#
+#  Code chunk 7
+#
+#=====================================================================================
+
+
+# Convert numeric lables into colors
+dynamicColors = labels2colors(dynamicMods)
+table(dynamicColors)
+# Plot the dendrogram and colors underneath
+pdf(file = "Plots/Root_Gene Dendogram with module colors.pdf", width = 12, height = 9);
+sizeGrWindow(8,6)
+plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05,
+                    main = "Gene dendrogram and module colors")
+dev.off()
+
+#=====================================================================================
+#
+#  Code chunk 8
+#
+#=====================================================================================
+
+
+# Calculate eigengenes
+MEList = moduleEigengenes(datExpr, colors = dynamicColors)
+MEs = MEList$eigengenes
+# Calculate dissimilarity of module eigengenes
+MEDiss = 1-cor(MEs, use="pairwise.complete.obs");
+# Cluster module eigengenes
+METree = hclust(as.dist(MEDiss), method = "average");
+# Plot the result
+pdf(file = "Plots/Root_ClusterinfofModuleEigengenes.pdf", width = 12, height = 9);
+sizeGrWindow(7, 6)
+plot(METree, main = "Clustering of module eigengenes",
+     xlab = "", sub = "")
+
+#=====================================================================================
+#
+#  Code chunk 9
+#
+#=====================================================================================
+
+
+MEDissThres = 0.25
+# Plot the cut line into the dendrogram
+abline(h=MEDissThres, col = "red")
+# Call an automatic merging function
+merge = mergeCloseModules(datExpr, dynamicColors, cutHeight = MEDissThres, verbose = 3)
+# The merged module colors
+mergedColors = merge$colors;
+# Eigengenes of the new merged modules:
+mergedMEs = merge$newMEs;
+dev.off()
+
+#=====================================================================================
+#
+#  Code chunk 10
+#
+#=====================================================================================
+
+
+sizeGrWindow(12, 9)
+pdf(file = "Plots/Root_geneDendro-3.pdf", wi = 9, he = 6)
+plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors),
+                    c("Dynamic Tree Cut", "Merged dynamic"),
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+dev.off()
+
+
+#=====================================================================================
+#
+#  Code chunk 11
+#
+#=====================================================================================
+
+
+# Rename to moduleColors
+moduleColors = mergedColors
+# Construct numerical labels corresponding to the colors
+colorOrder = c("grey", standardColors(50));
+moduleLabels = match(moduleColors, colorOrder)-1;
+MEs = mergedMEs;
+# Save module colors and labels for use in subsequent parts
+save(MEs, moduleLabels, moduleColors, geneTree, file = "Root-02-networkConstruction-stepByStep.RData")
+
